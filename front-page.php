4412a8250b7377eeda26c35f598b18457e2c9be8
TK <tasiukwaplong@gmail.com>
2024-11-03 20:34:37 +0100

    FT: Added video section

--- front-page.php
+++ front-page.php
@@ -5,61 +5,11 @@
 
         <!-- presidential candidate section -->
 		<?php get_template_part('template-parts/content', 'presidents'); ?>
+        <!-- Voter education -->
+		<?php get_template_part('template-parts/content', 'votereducation'); ?>
+		<?php get_template_part('template-parts/content', 'video'); ?>
     
-      
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 pt-3">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="<?php echo get_template_directory_uri();?>/assets/images/candidate1.png" class="w-100 candidate-img img-fluid rounded" alt="Candidate 1">
-                        </div>
-                        <div class="col-md-7">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugit aspernatur necessitatibus aliquam voluptatum soluta eveniet iure, deleniti quos ipsum suscipit perspiciatis animi laudantium ad sint iusto ratione nesciunt repellat.
-                            <p>
-                                <span class="float-right text-white p-1 rounded bg-danger">236</span>
-                                <span class="float-left">April, 2024</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-12 pt-3">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="<?php echo get_template_directory_uri();?>/assets/images/candidate1.png" class="w-100 candidate-img img-fluid rounded" alt="Candidate 1">
-                        </div>
-                        <div class="col-md-7">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugit aspernatur necessitatibus aliquam voluptatum soluta eveniet iure, deleniti quos ipsum suscipit perspiciatis animi laudantium ad sint iusto ratione nesciunt repellat.
-                            <p>
-                                <span class="float-right text-white p-1 rounded bg-danger">236</span>
-                                <span class="float-left">April, 2024</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-12 pt-3">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="<?php echo get_template_directory_uri();?>/assets/images/candidate1.png" class="w-100 candidate-img img-fluid rounded" alt="Candidate 1">
-                        </div>
-                        <div class="col-md-7">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugit aspernatur necessitatibus aliquam voluptatum soluta eveniet iure, deleniti quos ipsum suscipit perspiciatis animi laudantium ad sint iusto ratione nesciunt repellat.
-                            <p>
-                                <span class="float-right text-white p-1 rounded bg-danger">236</span>
-                                <span class="float-left">April, 2024</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-
-
-
-            </div>
-        </div>
+    
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
